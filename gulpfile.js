8c10391d4ce7f7ef08cb652cfac931e474019fae
Sam Lau <sam.suifai.lau@gmail.com>
2015-08-01 13:22:32 -0700

    added comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
+;(function() {
 'use strict';
 
+// Deps
 var fs = require('fs');
 var path = require('path');
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var del = require('del');
 var runSequence = require('run-sequence');
+// specify a different source folder to build
+var gulpSrc = process.env.GULP_SRC || 'src';
 
+// Configs
 var config = {
-    src: 'src',
+    src: gulpSrc,
     dist: 'dist',
     tmp: '.tmp',
     errorHandler: function(name) {
@@ -19,13 +24,19 @@ var config = {
     }
 };
 
-// load each gulp task in gulp folder
+// Load each gulp task in gulp folder
 fs.readdirSync(path.resolve(__dirname, 'gulp')).forEach(function(file) {
     if (file.charAt(0) === '.') return;
     var requirePath = path.resolve(__dirname, 'gulp', file);
     require(requirePath)(config);
 })
 
+/**
+ * gulp serve
+ * starts browserSync and starts watch
+ */
 gulp.task('serve', ['server', 'styles', 'javascript', 'inject'], function() {
     gulp.start('watch');
 });
+
+}());
